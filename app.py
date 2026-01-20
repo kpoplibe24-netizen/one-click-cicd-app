1c63df6be235ec60c7170b490f4d577498aaaa46
khushi <kpopli_be24@thapar.edu>
2026-01-20 19:26:53 +0530

    Add Flask App and requirements

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This app was deployed using a devOps pipeline"
+
+if __name__=="__main__":
+    app.run(debug=True)
